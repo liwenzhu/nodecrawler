f60e12693e58d760522e61a1a3837cca0a6cc8b3
liwenzhu <lee.wenzhu@gmail.com>
2014-08-26 14:44:33 +0800

    crawler implementation.

--- index.js
+++ index.js
@@ -1,37 +1,101 @@
-var Crawler = require("crawler").Crawler;
+var Bloom = require('bloom-lite');
+var showmemory = require('showmemory');
+var jschardet = require('jschardet');
+var iconv = require('iconv-lite');
+var htmlparser = require('htmlparser2');
+var http = require('http');
+var url = require('url');
+showmemory();
 
-var c = new Crawler({
-"maxConnections":10,
+var PORTAL = "http://www.jd.com";
+var bloom = new Bloom();
+var urls = [];
+var options = {};
+var count = 0;
+var INTERVAL = 100;
+var buffers = [], buf, encoding;
 
-// This will be called for each crawled page
-"callback":function(error,result,$) {
-
-    // $ is a jQuery instance scoped to the server-side DOM of the page
-    $("#content a").each(function(index,a) {
-        c.queue(a.href);
-    });
-}
+var parser = new htmlparser.Parser({
+	onopentag: function (name, attribs) {
+		if(name === "a") {
+			if(!bloom.exist(attribs.href)) {
+				count++;
+				if ( (count & 511) == 0) {
+					console.log("total pages: %s, url: %s", count, attribs.href);
+				}
+				urls.push(attribs.href);
+			}
+		}
+	}
 });
 
-// Queue just one URL, with default callback
-c.queue("http://joshfire.com");
+var crawl = function (crawlURL) {
+	if (!crawlURL) return process.exit(0);
+	var parsedUrl = url.parse(crawlURL);
+	if ("https:" === parsedUrl.protocol || !parsedUrl.protocol)
+		return crawl(urls.pop());
+	//extract url
+	options.hostname = parsedUrl.hostname;
+	options.path = parsedUrl.path;
 
-// Queue a list of URLs
-c.queue(["http://jamendo.com/","http://tedxparis.com"]);
+	http.request(options, function (res) {
+		res.on('data', function (chunk) {
+			buffers.push(chunk);
+		});
 
-// Queue URLs with custom callbacks & parameters
-c.queue([{
-"uri":"http://parishackers.org/",
-"jQuery":false,
+		res.on('end', function () {
+			buf = buf = Buffer.concat(buffers);
+			// console.log(jschardet.detect(buf));
+			encoding = jschardet.detect(buf).encoding;
+			body = iconv.decode(buf, encoding);
+			parser.write(body);
+			parser.end();
+			console.log("url list length:", urls.length);
+			setTimeout(function(){
+				crawl(urls.pop());
+			}, INTERVAL);
+		});
+	}).end();
+};
 
-// The global callback won't be called
-"callback":function(error,result) {
-	console.log(result.body);
-    console.log("Grabbed",result.body.length,"bytes");
-}
-}]);
+crawl(PORTAL);
 
-// Queue some HTML code directly without grabbing (mostly for tests)
-c.queue([{
-"html":"<p>This is a <strong>test</strong></p>"
-}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
