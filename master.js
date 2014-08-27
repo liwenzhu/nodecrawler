bff179e9c36a830acb4eed5e3e017046d5b5a8a2
liwenzhu <lee.wenzhu@gmail.com>
2014-08-27 15:13:11 +0800

    create a crawler cluster.

--- master.js
+++ master.js
@@ -1,6 +1,76 @@
+'use strict';
+
 var cluster = require('cluster');
 var numCPUs = require('os').cpus().length;
+var Bloom = require('bloom-lite');
+var bloom = new Bloom();
+
+var urls = [], count = 0;
+var LOG_INTERVAL = 10000; // 10 seconds
+
+var PORTAL_URL = "http://www.jd.com";
 
 cluster.setupMaster({
 	exec: "worker.js"
 });
+
+for (var i = 0; i < numCPUs; i++) {
+	createWorker();
+}
+
+function createWorker () {
+	var worker = cluster.fork();
+	worker.on('message', function (msg) {
+		if ((urls.length & 1023) === 0)
+			console.log("----------> URL: %s, %s", urls[0], new Date());
+		if (msg.cmd && msg.cmd === "url") {
+			if (!bloom.exist(msg.url)) {
+				count++;
+				bloom.add(msg.url);
+				urls.push(msg.url);
+			}
+		} else {
+			if(urls.length === 0)
+				console.log('urls is empty');
+			worker.send({url: urls.shift()});
+ 		}	
+	});
+	if (urls.length === 0)
+		worker.send({url: PORTAL_URL, id: worker.id});
+	else 
+		worker.send({url: urls.shift()});
+};
+
+// cluster.on('message', function (msg) {
+// 	if (msg.cmd && msg.cmd === "url") {
+// 		if (!bloom.exist(msg.url)) {
+// 			bloom.add(msg.url);
+// 			urls.push(msg.url);
+// 		}
+// 	} else {
+// 		if(urls.length === 0)
+// 			console.log('urls is empty');
+// 		cluster.workers[msg.id].send({url: urls.pop(), msg.id});
+// 	}
+// })
+
+cluster.on('online', function (worker) {
+	console.log('worker %s is ready.', worker.id);
+});
+
+cluster.on('exit', function (worker, code, signal) {
+	console.log('worker %d died (%s). restarting...',
+	worker.process.pid, signal || code);
+	createWorker();
+});
+
+
+function printUrlsLength() {
+	console.log("crawled pages:", count, new Date());
+	console.log("urls to crawl:", urls.length);
+	setTimeout(function () {
+		printUrlsLength();
+	}, LOG_INTERVAL);
+};
+
+printUrlsLength();
--- worker.js
+++ worker.js
@@ -1,50 +1,49 @@
 'use strict';
 
-var Bloom = require('bloom-lite');
-var showmemory = require('showmemory');
 var jschardet = require('jschardet');
 var iconv = require('iconv-lite');
 var htmlparser = require('htmlparser2');
 var http = require('http');
 var url = require('url');
-// showmemory(10000);
 
-var PORTAL = "http://www.jd.com";
-var bloom = new Bloom();
-var urls = [];
 var options = {
 	agent: false
 };
-var count = 0;
-var LOG_INTERVAL = 10000; // 10 seconds
 
 var buffers, buf, encoding, body, req;
+
+process.on('message', handleMessage);
+
+function handleMessage (msg) {
+	if (msg.url) {
+		crawl(msg.url, msg.id);
+	}
+};
 
 var parser = new htmlparser.Parser({
 	onopentag: function (name, attribs) {
 		if(name === "a" && attribs.href) {
-			if(!bloom.exist(attribs.href) 
-				// && (url.parse(attribs.href).protocol != null) 
-				&& attribs.href.indexOf('jd.com') > 0) {
-				count++;
-				urls.push(attribs.href);
+			if(attribs.href.indexOf('jd.com') > 0) {
+				process.send({cmd: 'url', url: attribs.href});
 			}
 		}
 	}
 });
 
-var crawl = function (crawlURL) {
-	if ( (count & 127) == 0)
-		console.log("----------> URL: %s, %s", crawlURL, new Date());
-	if (!crawlURL) return process.exit(0);
+var crawl = function (crawlURL, id) {
+	if (!crawlURL) {
+		console.log('#### crawlURL is null');
+		return setTimeout(function(){
+			process.send({});
+		}, 5000);
+	}
 	var parsedUrl = url.parse(crawlURL);
 	if ("https:" === parsedUrl.protocol || !parsedUrl.protocol)
-		return crawl(urls.pop());
+		return process.send({});
 	//extract url
 	options.hostname = parsedUrl.hostname;
 	options.path = parsedUrl.path;
 	buffers = [];
-	bloom.add(crawlURL);
 
 	req = http.request(options, function (res) {
 		res.on('data', function (chunk) {
@@ -55,15 +54,13 @@ var crawl = function (crawlURL) {
 			buf = Buffer.concat(buffers);
 			encoding = jschardet.detect(buf).encoding;
 			if(!encoding)
-				return crawl(urls.pop());
+				return process.send({});
 			body = iconv.decode(buf, encoding);
 			parser.write(body);
 			parser.end();
 			buf = null;
 			buffers = null;
-			process.nextTick(function () {
-				crawl(urls.pop());
-			});
+			process.send({});
 		});
 
 		res.on('error', function(err){
@@ -73,65 +70,13 @@ var crawl = function (crawlURL) {
 
 	req.setTimeout(30000, function(){
 		console.log(new Date(), 'request timeout, skip url:', crawlURL);
-		crawl(urls.pop());
+		process.send({});
 	});
 
 	req.on('error', function (e) {
 		console.log(new Date() , ' connection problem:', e, crawlURL);
-		crawl(urls.pop());
+		process.send({});
 	});
 
 	req.end();
 };
-
-function printUrlsLength() {
-	console.log("crawled pages:", count, new Date());
-	console.log("urls to crawl:", urls.length);
-	setTimeout(function () {
-		printUrlsLength();
-	}, LOG_INTERVAL);
-};
-
-printUrlsLength();
-
-crawl(PORTAL);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
